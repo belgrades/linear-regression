c3b0b5134c8aeee549ecd6f0f96622bacc6787b2
belgrades <fcremaldc@gmail.com>
2016-10-29 20:50:30 -0700

    Preprocessing column. Normalize columns but Price.

--- 1743734-run.py
+++ 1743734-run.py
@@ -1,11 +1,33 @@
 import importlib
 import numpy as np
+import pandas as pd
 
-lib = importlib.import_module("1743734-lib")
 
-a, b = 2.0, 4.0
+def preprocessing(df):
+    columns = ['Price', 'WinterRain', 'AGST', 'HarvestRain', 'Age']
+    df = df[columns]
+    price = df['Price']
+    columns.remove('Price')
+    df = df[columns].apply(lambda x: (x - x.mean()) / x.std())
+    df['Price'] = price
 
-x, y = lib.gendata(np.array([b, a]), 200)
-print(lib.descent(y, x, 0.0001, 100))
-lib.genplot(x, y, a=a, b=b)
+    return(df)
 
+
+def main():
+    lib = importlib.import_module("1743734-lib")
+    df = pd.read_csv('wine.csv')
+    df = preprocessing(df)
+    col = df.columns.tolist()
+    col.remove('Price')
+    df[col].apply(lambda x: (x - x.mean()) / x.std())
+    print(df.head())
+
+    a, b = 2.0, 4.0
+
+    x, y = lib.gendata(np.array([b, a]), 200)
+    print(lib.descent(y, x, 0.0001, 100))
+    lib.genplot(x, y, a=a, b=b)
+
+if __name__ == '__main__':
+    main()
