4305f367d392af3067dbfd5aa640a0787e28e288
belgrades <fcremaldc@gmail.com>
2016-11-06 09:40:23 -0800

    Changing parameters

--- 1743734-run.py
+++ 1743734-run.py
@@ -63,9 +63,9 @@ def main():
 
     w = lib.descent(y=y,
                     x=x,
-                    alpha=2e-2,
-                    itr=2e2,
-                    eps=1e-9)
+                    alpha=0.02235,
+                    itr=1e2,
+                    eps=1e-3)
     print(w)
     print(lib.r2(y=y, c=x, x=w))
 
@@ -75,9 +75,9 @@ def main():
 
     w = lib.descent(y=np.array(df['Price'].values),
                     x=x,
-                    alpha=2e-2,
-                    itr=2e2,
-                    eps=1e-9)
+                    alpha=0.02235,
+                    itr=1e2,
+                    eps=1e-3)
     print(w)
     print(lib.r2(y=y, c=x, x=w))
 
